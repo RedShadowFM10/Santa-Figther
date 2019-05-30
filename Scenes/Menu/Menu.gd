6b5c76835e16454530147ed9e449ce4d1d9b1ce5
RedShadowFM10 <julianlv0410@gmail.com>
2019-05-30 18:10:46 -0500

    Alpha v0.1 (Terminada)

--- Scenes/Menu/Menu.gd
+++ Scenes/Menu/Menu.gd
@@ -6,7 +6,10 @@ func _ready():
 
 # Boton de play
 func _on_Play_pressed():
-	Hide_Show(1)
+	Global.level = "01"
+	Global.Powers()
+	Fade_In_Out.change_scene("res://Scenes/Intro/Intro.tscn")
+#	Hide_Show(1)
 
 # Boton de los creditos
 func _on_Credits_Button_pressed():
@@ -17,16 +20,26 @@ func _on_Exit_Game_pressed():
 	get_tree().quit()
 
 func Hide_Show(kind):
-	$Tittle.visible = !$Tittle.visible
-	$Play.visible = !$Play.visible
-	$Credits_Button.visible = !$Credits_Button.visible
-	$Exit_Game.visible = !$Exit_Game.visible
-	$Version.visible = !$Version.visible
-	$Back.visible = !$Back.visible
 	if kind == 1:
+		$Tittle.visible = !$Tittle.visible
+		$Play.visible = !$Play.visible
+		$Credits_Button.visible = !$Credits_Button.visible
+		$Exit_Game.visible = !$Exit_Game.visible
+		$Back.visible = !$Back.visible
 		$Levels.visible = !$Levels.visible
+		$Version.visible = !$Version.visible
 	elif kind == 2:
+		$Tittle.visible = !$Tittle.visible
+		$Play.visible = !$Play.visible
+		$Credits_Button.visible = !$Credits_Button.visible
+		$Exit_Game.visible = !$Exit_Game.visible
+		$Back.visible = !$Back.visible
 		$Credits.visible = !$Credits.visible
+		$Asset_Credits.visible = !$Asset_Credits.visible
+	elif kind == 3:
+		$Credits.visible = !$Credits.visible
+		$Asset_Credits.visible = !$Asset_Credits.visible
+		$Assets_Credits_Text.visible = !$Assets_Credits_Text.visible
 
 #1 Boton de regreso
 func _on_Back_pressed():
@@ -35,6 +48,8 @@ func _on_Back_pressed():
 		Hide_Show(1)
 	elif $Credits.visible:
 		Hide_Show(2)
+	elif $Assets_Credits_Text.visible:
+		Hide_Show(3)
 
 #2
 func _on_Back_mouse_entered():
@@ -43,3 +58,6 @@ func _on_Back_mouse_entered():
 #3
 func _on_Back_mouse_exited():
 	$Back.modulate = Color(1, 1, 1)
+
+func _on_Asset_Credits_pressed():
+	Hide_Show(3)
